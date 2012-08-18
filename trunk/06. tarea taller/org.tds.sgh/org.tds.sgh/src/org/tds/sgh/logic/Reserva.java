9f9273938cc3b73c66273bd46644e706c6362ab3
marel.oliva@gmail.com <marel.oliva@gmail.com@dc24dd3d-b498-87d4-1d4f-b53fb2bb80fb>
2012-08-18 16:02:46 +0000

    implementacion de propiedades de Reserva

--- trunk/06. tarea taller/org.tds.sgh/org.tds.sgh/src/org/tds/sgh/logic/Reserva.java
+++ trunk/06. tarea taller/org.tds.sgh/org.tds.sgh/src/org/tds/sgh/logic/Reserva.java
@@ -4,46 +4,52 @@ import java.util.GregorianCalendar;
 
 public class Reserva implements IDatosReserva {
 
+	private long codigo;
+	private GregorianCalendar fechaInicio;
+	private GregorianCalendar fechaFin;
+	private boolean isModificablePorHuesped ;
+	private boolean isPendiente;
+	private boolean isTomada;
+	private boolean isCancelada;
+	
+
+	
 	@Override
 	public long getCodigo() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.codigo;
 	}
-
+	
 	@Override
 	public GregorianCalendar getFechaInicio() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.fechaInicio;
 	}
 
 	@Override
 	public GregorianCalendar getFechaFin() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.fechaFin;
 	}
 
 	@Override
 	public boolean isModificablePorHuesped() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.isModificablePorHuesped;
 	}
 
 	@Override
 	public boolean isPendiente() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.isPendiente;
 	}
 
 	@Override
 	public boolean isTomada() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.isTomada;
 	}
 
 	@Override
 	public boolean isCancelada() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.isCancelada;
 	}
-
+	
+	
 }
