f96602a9aa65fe0eddc3d2535c58a99d6c310a85
marel.oliva@gmail.com <marel.oliva@gmail.com@dc24dd3d-b498-87d4-1d4f-b53fb2bb80fb>
2012-08-18 16:36:52 +0000

    buscarReservasNoTomadas y Disponibilidad compilando.

--- trunk/06. tarea taller/org.tds.sgh/org.tds.sgh/src/org/tds/sgh/logic/Hotel.java
+++ trunk/06. tarea taller/org.tds.sgh/org.tds.sgh/src/org/tds/sgh/logic/Hotel.java
@@ -59,24 +59,52 @@ public class Hotel implements IDatosHotel
 		return habitaciones.get(nombreHabitacion).getTipoHabitacion();
 	}
 	
+	// MAREL
 	boolean confirmarDisponibilidad(String nombreTipoHabitacion, GregorianCalendar fechaInicio, GregorianCalendar fechaFin) {
 		boolean res = true;
 		
-		for ( IDatosReserva reserva: reservas ) {
+		int cantidadReservas = this.cantidadReservasTipoHabitacion(nombreTipoHabitacion, fechaInicio, fechaFin);
+		int cantidadaHabitaciones = this.cantidadHabitacionessTipoHabitacion(nombreTipoHabitacion);
 			
-		}
-		
-		return true;
+		return cantidadaHabitaciones > cantidadReservas;
 	}
 	
-
+	// MAREL
+	private int cantidadReservasTipoHabitacion(String nombreTipoHabitacion, GregorianCalendar fechaInicio, GregorianCalendar fechaFin) {		
+		int res = 0;
+		
+		for ( Reserva reserva: reservas ) {
+			if ( reserva.getTipoHabitacion().getNombre().equals(nombreTipoHabitacion) && 
+					reserva.getFechaInicio().after(fechaInicio)&&
+					reserva.getFechaFin().before(fechaFin)){
+				res+=1;
+			}
+		}
+		
+		return res;
+	}
+	
+	// MAREL
+	private int cantidadHabitacionessTipoHabitacion(String nombreTipoHabitacion) {
+		int res = 0;
+		
+		for (Habitacion habitacion : habitaciones.values()) {
+			if (habitacion.getTipoHabitacion().equals(nombreTipoHabitacion)) {
+				res += 1;
+			}
+		}
+		
+		return res;
+	}
+	
+	// MAREL
 	public List<IDatosHotel> sugerirAlternativas(String nombreTipoHabitacion,
 			GregorianCalendar fechaInicio, GregorianCalendar fechaFin) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
+	// MAREL
 	public IDatosReserva registrarReserva(String nombreCliente,
 			String nombreHotel, String nombreTipoHabitacion,
 			GregorianCalendar fechaInicio, GregorianCalendar fechaFin,
@@ -85,22 +113,28 @@ public class Hotel implements IDatosHotel
 		return null;
 	}
 
+
+	
 	/**
-	 * Alvaro Jose Peralta Ocampo
-	 * @param fecha
-	 * @return
-	 */
-	public List<IDatosReserva> buscarReservasNoTomadas(GregorianCalendar fecha) {
-		List<IDatosReserva> datosReservas = new ArrayList<IDatosReserva>();
-		Iterator<Reserva> iterReservas = reservas.iterator();
-		while(iterReservas.hasNext()){
-			Reserva reserva = iterReservas.next();
-			boolean enFechayNoTomada = reserva.estasEnFechayNoTomada(fecha);
-			if(enFechayNoTomada){
-				IDatosReserva iDatosReserva = new ReservaDTO(reserva);
-				
-			}
-		}
-		return datosReservas;
-	}
+     * Alvaro Jose Peralta Ocampo
+     *
+     * @param fecha
+     * @return
+     */
+    public List<IDatosReserva> buscarReservasNoTomadas(GregorianCalendar fecha) {
+            List<IDatosReserva> datosReservas = new ArrayList<IDatosReserva>();
+            Iterator<Reserva> iterReservas = reservas.iterator();
+            while (iterReservas.hasNext()) {
+                    Reserva reserva = iterReservas.next();
+                    boolean enFechayNoTomada = reserva.estasEnFechayNoTomada(fecha);
+                    if (enFechayNoTomada) {
+                            IDatosReserva iDatosReserva = new ReservaDTO(reserva);
+                            datosReservas.add(iDatosReserva);
+                    }
+            }
+            return datosReservas;
+    }
+
+	
+
 }
